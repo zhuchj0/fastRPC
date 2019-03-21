296af535fde60612f537822878df91a74031937b
zcj12396 <42466067+zcj12396@users.noreply.github.com>
2019-03-21 11:52:19 +0800

    Update FastRpcApplication.java

--- src/main/java/com/fastRPC/FastRpcApplication.java
+++ src/main/java/com/fastRPC/FastRpcApplication.java
@@ -1,29 +1,29 @@
 package com.fastRPC;
 
 import com.fastRPC.bootstrap.FastRpcClientBootstrap;
+import com.fastRPC.bootstrap.FastRpcServerBootstrap;
 import com.fastRPC.client.core.ProxyFactory;
 import com.fastRPC.client.service.Echo2;
-import com.fastRPC.client.zookeeper.NamingManager;
 
 
 public class FastRpcApplication {
 
     public static void main(String[] args) throws Exception {
 
-//         FastRpcClientBootstrap.start();
-//         Echo2 service = ProxyFactory.getInstance(Echo2.class);
-//         //计时500次
-//         Long start = System.currentTimeMillis();
-//         int count = 10;
-//         for(int i=0;i<count;i++){
-//             //实际调用
-//             service.echotry();
+//        FastRpcClientBootstrap.start();
+//        Echo2 service = ProxyFactory.getInstance(Echo2.class);
+//        //计时500次
+//        Long start = System.currentTimeMillis();
+//        int count = 10;
+//        for(int i=0;i<count;i++){
+//            //实际调用
+//            service.echotry();
+//
+//        }
+//        Long pass = (System.currentTimeMillis()-start)/1000;
+//        System.out.println("执行"+count+"次用时"+pass+"秒");
 
-//         }
-//         Long pass = (System.currentTimeMillis()-start)/1000;
-//         System.out.println("执行"+count+"次用时"+pass+"秒");
-        
-       FastRpcServerBootstrap.start();
+        FastRpcServerBootstrap.start();
 
 
     }
