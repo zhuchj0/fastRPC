7a92293cb656c6a5a88f64ad966b229ea2449cac
zcj12396 <42466067+zcj12396@users.noreply.github.com>
2019-03-21 10:54:57 +0800

    Update FastRpcApplication.java

--- src/main/java/com/fastRPC/FastRpcApplication.java
+++ src/main/java/com/fastRPC/FastRpcApplication.java
@@ -10,20 +10,20 @@ public class FastRpcApplication {
 
     public static void main(String[] args) throws Exception {
 
-        FastRpcClientBootstrap.start();
-        Echo2 service = ProxyFactory.getInstance(Echo2.class);
-        //计时500次
-        Long start = System.currentTimeMillis();
-        int count = 10;
-        for(int i=0;i<count;i++){
-            //实际调用
-            service.echotry();
+//         FastRpcClientBootstrap.start();
+//         Echo2 service = ProxyFactory.getInstance(Echo2.class);
+//         //计时500次
+//         Long start = System.currentTimeMillis();
+//         int count = 10;
+//         for(int i=0;i<count;i++){
+//             //实际调用
+//             service.echotry();
 
-        }
-        Long pass = (System.currentTimeMillis()-start)/1000;
-        System.out.println("执行"+count+"次用时"+pass+"秒");
+//         }
+//         Long pass = (System.currentTimeMillis()-start)/1000;
+//         System.out.println("执行"+count+"次用时"+pass+"秒");
         
-//        FastRpcServerBootstrap.start();
+       FastRpcServerBootstrap.start();
 
 
     }
